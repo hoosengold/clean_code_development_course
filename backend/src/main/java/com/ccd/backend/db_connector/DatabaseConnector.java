23609a5a654559ec49f16c7b41283ba373e79b9e
Ivan Dimov <ivan_dimov8@abv.bg>
2024-02-29 10:28:43 +0200

    Fixed database connection interface

--- backend/src/main/java/com/ccd/backend/db_connector/DatabaseConnector.java
+++ backend/src/main/java/com/ccd/backend/db_connector/DatabaseConnector.java
@@ -1,17 +1,26 @@
+package com.ccd.backend.db_connector;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnector {
-    private String JDBC_URL = "jdbc:h2:mem:test_db";
-    private String USER = "sa";
-    private String PASSWORD = "";
+    private final String jdbcUrl;
+    private final String dbUser;
+    private final String dbPassword;
     private Connection connection;
+
+    public DatabaseConnector() {
+        this.jdbcUrl = System.getenv("JDBC_URL");
+        this.dbUser = System.getenv("DB_USER");
+        this.dbPassword = System.getenv("DB_PASSWORD");
+    }
+
 
     public void openConnection() {
         try {
             Class.forName("org.h2.Driver");
-            connection = DriverManager.getConnection(JDBC_URL, USER, PASSWORD);
+            connection = DriverManager.getConnection(this.jdbcUrl, this.dbUser, this.dbPassword);
             System.out.println("Connected to the database.");
         } catch (ClassNotFoundException | SQLException e) {
             System.err.println("Failed to connect to the database: " + e.getMessage());
