f4b57155576259156cd28971218d9bf59b324f8c
AlexanderRuskov <aleksander.ruskov@fdiba.tu-sofia.bg>
2024-03-23 11:45:01 +0200

    Implement Get Rest point for user register(#17).

--- backend/src/main/java/com/ccd/backend/RestAPI/UserController.java
+++ backend/src/main/java/com/ccd/backend/RestAPI/UserController.java
@@ -1,12 +1,18 @@
+package com.ccd.backend.RestAPI;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//@RequestMapping("/api/test")
 public class UserController {
 
+
+    @CrossOrigin
     @GetMapping("/register")
     public ResponseEntity<String> registerUser(
         @RequestParam String username,
@@ -14,9 +20,11 @@ public class UserController {
         @RequestParam String password) {
 
 
+
         if (username == null || email == null || password == null) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Missing parameters");
         }
+
 
         return ResponseEntity.status(HttpStatus.OK).body("User registered successfully");
     }
