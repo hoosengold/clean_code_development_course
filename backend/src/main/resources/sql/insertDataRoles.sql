aaa7a8adb24cb5db0af41becf97eb38b6f36e260
hoosengold <73896404+hoosengold@users.noreply.github.com>
2024-03-31 13:03:21 +0300

    Refactor the SQL scripts (#61)

--- backend/src/main/resources/sql/insertDataRoles.sql
+++ backend/src/main/resources/sql/insertDataRoles.sql
@@ -1,4 +1,4 @@
-INSERT INTO PUBLIC.Roles (Name, Description) VALUES
+INSERT INTO PUBLIC.ROLES (NAME, DESCRIPTION) VALUES
     ('User', 'A regular user who can play games and view the leaderboard'),
     ('Admin', 'An administrator who can manage the site and users'),
     ('Moderator', 'A moderator who can manage users and view all game stats');
