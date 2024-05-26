9b0ce32325ddc5456587de7305894dc8d489ddcc
hoosengold <73896404+hoosengold@users.noreply.github.com>
2024-05-26 22:17:24 +0300

    Fix ApplicationUserSerivice is already defined error

--- backend/src/main/java/com/ccd/backend/service/user/ApplicationUserService.java
+++ backend/src/main/java/com/ccd/backend/service/user/ApplicationUserService.java
@@ -4,7 +4,6 @@ import com.ccd.backend.db_connector.DatabaseConnector;
 import com.ccd.backend.entity.ApplicationUser;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +11,6 @@ import java.util.Objects;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
 @AllArgsConstructor
 @NoArgsConstructor
 public class ApplicationUserService implements UserService {
